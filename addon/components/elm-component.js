4c52b994827c6c669eabc744428e983f0367dbaf
John Landis <jlandis@bcorporation.net>
2022-11-06 08:13:02 -0500

    cleaner

--- addon/components/elm-component.js
+++ addon/components/elm-component.js
@@ -2,33 +2,17 @@ import { action } from '@ember/object';
 import Component from '@glimmer/component';
 
 export default class ElmComponent extends Component {
-  src = undefined;
-
-  // anything you want to pass to the Elm module
-  flags = undefined;
-
-  // function that is passed the Elm module's ports
-  setup = () => {}
-
-  constructor(owner, args) {
-    super(owner, args);
-
-    this.src = args.src;
-    this.flags = args.flags;
-    this.setup = args.setup;
-  }
-
   @action
   insertedElement(element) {    
-    if (!this.src) {
+    if (!this.args.src) {
       throw new Error('elm-component missing src object');
     }
 
-    const { ports } = this.src.init({
+    const { ports } = this.args.src.init({
       node: element,
-      flags: this.flags
+      flags: this.args.flags
     });
 
-    this.setup(ports);
+    this.args.setup(ports);
   }
 }
