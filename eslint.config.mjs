6b6c5f60bb8b323f43b89ab864e64ecfc109c4ba
John Landis <jalandis@users.noreply.github.com>
2025-01-06 13:57:52 -0500

    eslint 9 upgrade (#106)

---
+++ eslint.config.mjs
@@ -0,0 +1,89 @@
+import ember from "eslint-plugin-ember";
+import babel from "@babel/eslint-plugin";
+import globals from "globals";
+import babelParser from "@babel/eslint-parser";
+import node from "eslint-plugin-node";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+export default [{
+    ignores: [
+        "blueprints/*/files/",
+        "vendor/",
+        "dist/",
+        "tmp/",
+        "docs/",
+        "bower_components/",
+        "coverage/",
+        ".node_modules.ember-try/",
+        "bower.json.ember-try",
+        "package.json.ember-try",
+    ],
+}, ...compat.extends("eslint:recommended", "plugin:ember/recommended"), {
+    plugins: {
+        ember,
+        "@babel": babel,
+    },
+
+    languageOptions: {
+        globals: {
+            ...globals.browser,
+        },
+
+        parser: babelParser,
+        ecmaVersion: 2017,
+        sourceType: "module",
+
+        parserOptions: {
+            requireConfigFile: false,
+
+            babelOptions: {
+                plugins: [["@babel/plugin-proposal-decorators", {
+                    decoratorsBeforeExport: true,
+                }]],
+            },
+        },
+    },
+
+    rules: {
+        "ember/no-classic-components": "off",
+        "ember/no-component-lifecycle-hooks": "off",
+    },
+}, {
+    files: [
+        "**/.template-lintrc.js",
+        "**/ember-cli-build.js",
+        "**/index.js",
+        "**/testem.js",
+        "blueprints/*/index.js",
+        "config/**/*.js",
+        "tests/dummy/config/**/*.js",
+        "**/.eslintrc.js",
+    ],
+
+    ignores: ["addon/**", "addon-test-support/**", "app/**", "tests/dummy/app/**"],
+
+    plugins: {
+        node,
+    },
+
+    languageOptions: {
+        globals: {
+            ...Object.fromEntries(Object.entries(globals.browser).map(([key]) => [key, "off"])),
+            ...globals.node,
+        },
+
+        ecmaVersion: 2015,
+        sourceType: "script",
+    },
+}];
